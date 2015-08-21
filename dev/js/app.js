df605b4b4c4596a80369f87aee210d141fd3b62b
Tyler van der Hoeven <hi@tyvdh.com>
2015-08-21 15:40:28 -0400

    allow string inputs in the calendar object

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,18 +1,18 @@
-new Calendar({
+var ss = new Calendar({
   element: $('.daterange--single'),
-  current_date: new Date('June 15, 2015')
+  current_date: 'June 15, 2015'
 });
   
-new Calendar({
+var dd = new Calendar({
   element: $('.daterange--double'),
-  earliest_date: moment(new Date('January 1, 2000')).startOf('day'),
-  latest_date: moment(new Date()).endOf('day'),
-  start_date: new Date('May 1, 2015'),
-  end_date: new Date('May 31, 2015'),
+  earliest_date: 'January 1, 2000',
+  latest_date: new Date(),
+  start_date: 'May 1, 2015',
+  end_date: 'May 31, 2015',
   callback: function() {
     var start = moment(this.start_date).format('ll'),
         end = moment(this.end_date).format('ll');
     
-    // console.log('Start Date: '+ start +'\nEnd Date: '+ end);
+    console.debug('Start Date: '+ start +'\nEnd Date: '+ end);
   }
 });
