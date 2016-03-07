b82ca0cad3bc1ca42b7faa3cf9975848d06e519e
Tyler van der Hoeven <hi@tyvdh.com>
2016-03-07 18:23:49 -0500

    Update 2016-03-07T23:23:47.660Z

--- js/app.js
+++ js/app.js
@@ -2,15 +2,58 @@ var ss = new Calendar({
   element: $('.daterange--single'),
   current_date: 'June 15, 2015',
   format: {input: 'M/D/YYYY'},
+  placeholder: 'Select a Date',
   required: false
 });
   
 var dd = new Calendar({
-  element: $('.daterange--double'),
+  element: $('.one'),
   earliest_date: 'January 1, 2000',
-  latest_date: new Date(),
-  start_date: 'May 1, 2015',
-  end_date: 'May 31, 2015',
+  latest_date: moment(),
+  start_date: moment().subtract(29, 'days'),
+  end_date: moment(),
+  callback: function() {
+    var start = moment(this.start_date).format('ll'),
+        end = moment(this.end_date).format('ll');
+    
+    console.debug('Start Date: '+ start +'\nEnd Date: '+ end);
+  }
+});
+
+new Calendar({
+  element: $('.two'),
+  earliest_date: 'January 1, 2000',
+  latest_date: moment(),
+  start_date: moment().subtract(29, 'days'),
+  end_date: moment(),
+  presets: false,
+  callback: function() {
+    var start = moment(this.start_date).format('ll'),
+        end = moment(this.end_date).format('ll');
+    
+    console.debug('Start Date: '+ start +'\nEnd Date: '+ end);
+  }
+});
+
+new Calendar({
+  element: $('.three'),
+  earliest_date: 'January 1, 2000',
+  latest_date: moment(),
+  start_date: moment().subtract(29, 'days'),
+  end_date: moment(),
+  presets: [{
+    label: 'Last 30 days',
+    start: moment().subtract(29, 'days'),
+    end: moment()
+  },{
+    label: 'Last month',
+    start: moment().subtract(1, 'month').startOf('month'),
+    end: moment().subtract(1, 'month').endOf('month')
+  },{
+    label: 'Last year',
+    start: moment().subtract(12, 'months').startOf('month'),
+    end: moment().subtract(1, 'month').endOf('month')
+  }],
   callback: function() {
     var start = moment(this.start_date).format('ll'),
         end = moment(this.end_date).format('ll');
