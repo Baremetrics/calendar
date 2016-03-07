2cb2a59615e0a54410eb868b037947bdf828e1a5
Roc Alayo <ralayo@bmat.com>
2016-03-07 17:00:10 +0100

    Base first day of week on the current locale used

--- dev/js/Calendar.js
+++ dev/js/Calendar.js
@@ -623,14 +623,16 @@
 
     var first_day = moment(switcher || current).startOf('month');
     var last_day = moment(switcher || current).endOf('month');
+    var current_month_start_day = +first_day.format('d') - moment().localeData().firstDayOfWeek();
+    var current_month_end_day = +last_day.format('d') - moment().localeData().firstDayOfWeek();
 
     var current_month = {
       start: {
-        day: +first_day.format('d'),
+        day: current_month_start_day ? current_month_start_day : 7 - current_month_start_day,
         str: +first_day.format('D')
       },
       end: {
-        day: +last_day.format('d'),
+        day: current_month_end_day ? current_month_end_day : 7 - current_month_end_day,
         str: +last_day.format('D')
       }
     }
@@ -707,8 +709,10 @@
   Calendar.prototype.calendarHTML = function(type) {
     var ul_days_of_the_week = $('<ul class="dr-days-of-week-list"></ul>')
     var self = this;
+    var days = this.days_array || moment.weekdaysMin();
+    days = days.splice(moment().localeData().firstDayOfWeek()).concat(days.splice(0, moment().localeData().firstDayOfWeek()));
 
-    $.each(this.days_array || moment.weekdaysMin(), function(i, elem) {
+    $.each(days, function(i, elem) {
       ul_days_of_the_week.append('<li class="dr-day-of-week">' + elem + '</li>'); 
     });
 
