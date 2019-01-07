087227adad9cf7c1a28331c386fe7c6e0be8db8a
Tyler van der Hoeven <hi@tyvdh.com>
2019-01-07 16:56:47 -0500

    updated to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -10,49 +10,59 @@ var gulp = require('gulp'),
 
 // ROOT TASKS // ---------------------------------------------------------
 // Main style task  
-gulp.task('css', function() {
-  return gulp.src('dev/sass/**/**.scss')
+function css(cb) {
+  gulp.src('dev/sass/**/**.scss')
     .pipe(sass())
     .on('error', handleError)
     .pipe(autoprefixer({cascade: false,  browsers: ['last 10 versions']})) // auto prefix
     .pipe(cleancss()) // minify everything
     .pipe(gulp.dest('public/css'));
-});
+
+  cb();
+}
 
 // Main Javascript task
-gulp.task('js', function() {  
-  return gulp.src('dev/js/**/*.js')
+function js(cb) {
+  gulp.src('dev/js/**/*.js')
     .pipe(newer('public/js'))
     // .pipe(uglify())
     .on('error', handleError)
     .pipe(gulp.dest('public/js'));
-});
+
+  cb();
+}
 
 // Publish github page
-gulp.task('deploy', function() {
-  return gulp.src('public/**/*')
+function deploy(cb) {
+  gulp.src('./public/**/*')
     .pipe(ghPages());
-});
+
+  cb();
+}
 
 
 // FUNCTIONS // ---------------------------------------------------------
 // Watch function
-gulp.task('watch', function() {
-  gulp.start('js', 'css');
-
-  gulp.watch('dev/sass/**/*.scss', ['css']);
-  gulp.watch('dev/js/**/*.js', ['js']);
-  gulp.watch('dev/img/**/*.{jpg,jpeg,png,gif,svg,ico}', ['img']);
+function watch(cb) {
+  gulp.watch('dev/js/**/*.js', js);
+  gulp.watch('dev/sass/**/*.scss', css);
+  // gulp.watch('dev/img/**/*.{jpg,jpeg,png,gif,svg,ico}', img);
  
   livereload.listen();
   gulp.watch(['public/*.html', 'public/js/**/*.js', 'public/img/**/*.{jpg,jpeg,png,gif,svg,ico}', 'public/css/*.css']).on('change', livereload.changed);
-});
 
-// Default function
-gulp.task('default', ['watch']);
+  cb();
+}
 
 // Error reporting function
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
+
+// Default function
+exports.default = gulp.series(js, css, watch)
+
+exports.css = css
+exports.js = js
+exports.deploy = deploy
